d0023fc0e916f255bc0a72b6100f20011a99210e
Lea Model <lea.skywalker309@gmail.com>
2023-07-10 16:06:39 +0200

    Iteration 2 done

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,12 @@
 //  Add your code here
+const { Schema, model } = require("mongoose")
+
+const celebritySchema = new Schema ({
+    name : {type: String, trim: true },
+    occupation : {type : String, default: 'unknown' },
+    catchPhrase : {type: String, required: true },
+})
+
+const CelebrityModel = model("Celebrity", celebritySchema)
+
+model.exports = CelebrityModel
